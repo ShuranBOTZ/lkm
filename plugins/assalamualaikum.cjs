6370971cafd165364a90f16eac16c66fa641e489
ShuranBOTZ <114658595+ShuranBOTZ@users.noreply.github.com>
2022-10-31 06:16:44 +0700

    Add files via upload

---
+++ plugins/assalamualaikum.cjs
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, participants }) => {
+ m.reply('Waalaikumsalam brader')
+      }
+           handler.help = ['Assalamualaikum']
+                handler.tags = ['quran']
+                     
+                          handler.customPrefix = /^Assalamualaikum|Assalamu'alaikum$/i
+                               handler.command = new RegExp
+                                    module.exports = handler
